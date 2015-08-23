1a915fbec07fe86d233dfddd1bdf0c1e2147a564
anonymframework <serif.saglam119@gmail.com>
2015-08-23 04:08:32 +0300

    added capsule interface

--- Capsule/CapsuleInterface.php
+++ Capsule/CapsuleInterface.php
@@ -9,9 +9,29 @@
  */
 
 namespace Anonym\Components\Database\Capsule;
+use Anonym\Components\Database\Base;
 
-
+/**
+ * Interface CapsuleInterface
+ * @package Anonym\Components\Database\Capsule
+ */
 interface CapsuleInterface
 {
 
+    /**
+     * add a connection to capsule
+     *
+     * @param Base $connection
+     * @param string $name
+     * @throws CapsuleInstanceException
+     */
+    public function addConnection(Base $connection = null, $name = '');
+
+    /**
+     * delete a connection in capsule
+     *
+     * @param mixed $offset
+     * @return $this
+     */
+    public function deleteConnection($offset);
 }
