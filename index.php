3393f34669688dd69e0421962ecfa51c31e8e53c
anonymframework <serif.saglam119@gmail.com>
2015-08-25 14:36:30 +0300

    updated index.php

--- index.php
+++ index.php
@@ -7,3 +7,14 @@
  *
  * Thanks for using
  */
+
+include 'vendor/autoload.php';
+$base = new \Anonym\Components\Database\Base(
+    [
+        'host'     => 'localhost',
+        'db'       => 'test',
+        'username' => 'root',
+        'password' => ''
+    ]
+);
+
