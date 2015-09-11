69aebc8d1328c8b3885ad7ea51d76affb253e260
anonymframework <serif.saglam119@outlook.com>
2015-09-11 12:42:42 +0300

    made some updates

--- Pagination/PaginationFactory.php
+++ Pagination/PaginationFactory.php
@@ -12,4 +12,23 @@ namespace Anonym\Components\Database\Pagination;
 class PaginationFactory
 {
 
+    /**
+     * @var int
+     */
+    protected $perPage;
+
+    /**
+     * the required parameter
+     *
+     * @var int|null
+     */
+    protected $currentPage;
+
+    /**
+     * the optional configs
+     *
+     * @var array
+     */
+    protected $options;
+
 }
