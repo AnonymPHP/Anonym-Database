daa17114a01035fc6747d4928df386cd844a2373
anonymframework <serif.saglam119@outlook.com>
2015-09-11 12:41:12 +0300

    made some updates

---
+++ Pagination/PaginationFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11.09.2015
+ * Time: 12:41
+ */
+
+namespace Anonym\Components\Database\Pagination;
+
+
+class PaginationFactory
+{
+
+}
