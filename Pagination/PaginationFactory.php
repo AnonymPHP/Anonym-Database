2761b5a95d71810abe171fd2121c9fa4ccd6a11d
anonymframework <serif.saglam119@outlook.com>
2015-09-11 12:42:53 +0300

    made some updates

--- Pagination/PaginationFactory.php
+++ Pagination/PaginationFactory.php
@@ -31,4 +31,58 @@ class PaginationFactory
      */
     protected $options;
 
+    /**
+     * @return int
+     */
+    public function getPerPage()
+    {
+        return $this->perPage;
+    }
+
+    /**
+     * @param int $perPage
+     * @return PaginationFactory
+     */
+    public function setPerPage($perPage)
+    {
+        $this->perPage = $perPage;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getCurrentPage()
+    {
+        return $this->currentPage;
+    }
+
+    /**
+     * @param int|null $currentPage
+     * @return PaginationFactory
+     */
+    public function setCurrentPage($currentPage)
+    {
+        $this->currentPage = $currentPage;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     * @return PaginationFactory
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+        return $this;
+    }
+
 }
