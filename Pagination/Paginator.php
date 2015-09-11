36a5c4faf2334b801689b5874e6548f96563cd2a
anonymframework <serif.saglam119@outlook.com>
2015-09-11 12:37:20 +0300

    added pagination class

---
+++ Pagination/Paginator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11.09.2015
+ * Time: 12:37
+ */
+
+namespace Anonym\Components\Database\Pagination;
+
+/**
+ * Class Paginator
+ * @package Anonym\Components\Database\Pagination
+ */
+class Paginator
+{
+
+}
