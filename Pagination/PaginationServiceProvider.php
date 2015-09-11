703f47fe8a432e64c785e5eb2eebe0d72293cdd3
anonymframework <serif.saglam119@outlook.com>
2015-09-12 00:30:35 +0300

    made some updates

--- Pagination/PaginationServiceProvider.php
+++ Pagination/PaginationServiceProvider.php
@@ -11,7 +11,40 @@
 namespace Anonym\Components\Database\Pagination;
 
 
-class PaginationServiceProvider
+use Anonym\Bootstrap\ServiceProvider;
+use Anonym\Components\HttpClient\Request;
+use Anonym\Facades\App;
+
+/**
+ * Class PaginationServiceProvider
+ * @package Anonym\Components\Database\Pagination
+ */
+class PaginationServiceProvider extends ServiceProvider
 {
 
+
+    /**
+     * register the provider
+     *
+     * @return mixed
+     */
+    public function register()
+    {
+        Paginator::setCurrentPageFinder(function(){
+            if (isset($_GET['page'])) {
+                $page = GUMP::xss_clean($_GET['page']);
+
+                return $page;
+            }else{
+                return 1;
+            }
+        });
+
+        $request = $this->make('http.request');
+        Paginator::getRequestPathFinder(function() use ($request){
+            if ($request instanceof Request) {
+                return $request->getBaseWithoutQuery();
+            }
+        });
+    }
 }
