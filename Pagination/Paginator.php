f9a507492ee54b9a6d4567e0a54e929f058f5cda
anonymframework <serif.saglam119@outlook.com>
2015-09-11 12:41:01 +0300

    made some updates

--- Pagination/Paginator.php
+++ Pagination/Paginator.php
@@ -12,7 +12,28 @@ namespace Anonym\Components\Database\Pagination;
  * Class Paginator
  * @package Anonym\Components\Database\Pagination
  */
-class Paginator
+class Paginator extends PaginationFactory
 {
+
+
+
+    /**
+     * determine if there are more page in datas
+     *
+     * @var int
+     */
+    protected $hasMore;
+
+    /**
+     * create a new instance and register options and more variables
+     *
+     * @param mixed $items
+     * @param int $perPage
+     * @param null $currentPage
+     * @param array $options
+     */
+    public function __construct($items, $perPage, $currentPage = null, array $options = []){
+
+    }
 
 }
